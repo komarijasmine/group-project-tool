77ca8cdc7c4df3b665efcff8b58dde83b80a3beb
Komari <138456103+komarijasmine@users.noreply.github.com>
2024-07-16 11:30:10 +0900

    fixed main.js

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+var nextID = 2
+
 function addInnerDiv() {
-    var container = document.getElementById("container")
-    container.innerHTML = "<div class ='button'> hi </div>"
+    var container = document.getElementById("todo_sections")
+    var todo_list = container.innerHTML
+    container.innerHTML = "<div class='todo_section' id='todo_" + nextID +"'></div>" + todo_list
+    nextID += 1
 }
+
+
