af8531c86b0f4694fe644051ec1a3191951b8500
Komari <138456103+komarijasmine@users.noreply.github.com>
2024-07-23 10:38:22 +0900

    todo now works!!

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-var nextID = 2
+var nextID = 1
 
-function addInnerDiv() {
-    var container = document.getElementById("todo_sections")
-    var todo_list = container.innerHTML
-    container.innerHTML = "<div class='todo_section' id='todo_" + nextID +"'></div>" + todo_list
-    nextID += 1
+function addTodoSection() {
+
+    var container = document.getElementById("todo_sections");
+    var todo_list = container.innerHTML;
+
+    container.innerHTML = todo_list + "<div class='todo_section' id='todo_" + nextID + "'>" +
+        "<h2>Unnamed To-Do Section " + nextID + "</h2>" + 
+        "<br><input type='checkbox'> " +
+        "<label>Task Description</label>" +
+        "<br>" +
+        "<input type='text' id='new-task-text' placeholder='New task description'>" + 
+        "<button onclick='addTask('planning')'>Add Task</button>" +
+        "</div>"
+    nextID += 1;
+
+
+}
+
+function addTask(categoryId) {
+    var category = document.getElementById(categoryId);
+    var newTaskText = document.getElementById('new-task-text').value;
+    var newTaskHtml = `
+        <input type="checkbox">
+            <label>${newTaskText}</label>
+        </div>`;
+    category.innerHTML += newTaskHtml;
+    document.getElementById('new-task-text').value = "";
 }
 
 
